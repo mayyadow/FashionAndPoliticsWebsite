e8a60d5d140d8f8635148d8e2eb1dbf9dce021cc
Maya Dowman <mayadowman2024@gmail.com>
2026-02-09 00:12:47 -0500

    Update import paths for DesktopAbout and MobileAbout

--- fashionandpoliticswebsite/app/about/page.tsx
+++ fashionandpoliticswebsite/app/about/page.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import DesktopAbout from "./about/DesktopAbout";
-import MobileAbout from "./about/MobileAbout";
+import DesktopAbout from "app/about/DesktopAbout";
+import MobileAbout from "app/about/MobileAbout";
 
 export default function AboutResponsive() {
   const [isMobile, setIsMobile] = useState(false);
